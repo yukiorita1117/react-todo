fd36846ce102cf2980dd100866525f1359fc3703
折田幸夫 <3204704@gmail.com>
2020-01-23 00:29:21 +0900

    react化

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+const App = props => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const increment = () => setPrice(price + 1);
+  const decrement = () => setPrice(price - 1);
+  const reset = () => setPrice(props.price);
+
+  return (
+    <>
+      <p>
+        現在の{name}は{price}円持っています。
+      </p>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>RESET</button>
+      <input value={name}></input>
+    </>
+  );
+};
+
+App.defaultProps = {
+  name: "JSON_HardCoder",
+  price: 1000
+};
+
+export default App;
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-const App: React.FC = () => {
-  return <></>;
-};
-
-export default App;
