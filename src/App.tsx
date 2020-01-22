f621a3df70f6e615e382a2a4e2be5b6e5c97b610
折田幸夫 <3204704@gmail.com>
2020-01-22 23:34:02 +0900

    create Counter

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import { useState } from "react";
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const increment2 = () => setCount(prevCount => prevCount + 1);
+  const decrement2 = () => setCount(prevCount => prevCount - 1);
 
   return (
     <>
       <div>count : {count}</div>
       <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <br />
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
     </>
   );
 };
