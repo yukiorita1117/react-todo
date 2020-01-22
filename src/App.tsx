45f3ca748cbde8deaa74ba82bb3c20cbe4922997
折田幸夫 <3204704@gmail.com>
2020-01-22 22:59:28 +0900

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {/* Edit <code>src/App.tsx</code> and save to reload. */}
-          Hello,World!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
