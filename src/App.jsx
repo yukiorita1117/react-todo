9e71500d44fd5980944a934d2032a1ba125388b4
折田幸夫 <3204704@gmail.com>
2020-01-23 21:58:58 +0900

    end useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import { useState } from "react";
 
 const App = props => {
   const [state, setState] = useState(props);
+  const { name, price } = state;
 
-  //初期化処理と同じことをすれば良い
-  const reset = () => {
-    setState(props);
-  };
-
+  // 初期化処理と同じことをすれば良い
+  // const reset = () => {
+  //   setState(props);
+  // };
   return (
     <>
       <p>
-        現在の{state.name}は{state.price}円持っています。
+        現在の{name}は{price}円持っています。
       </p>
       <button
         onClick={() => {
-          setState({ ...state, price: state.price + 1 });
+          setState({ ...state, price: price + 1 });
         }}
       >
         +1
       </button>
       <button
         onClick={() => {
-          setState({ ...state, price: state.price - 1 });
+          setState({ ...state, price: price - 1 });
         }}
       >
         -1
@@ -37,7 +37,7 @@ const App = props => {
       </button>
       {/* e.targetで入力値を拾える */}
       <input
-        value={state.name}
+        value={name}
         onChange={e => setState({ ...state, name: e.target.value })}
       ></input>
     </>
