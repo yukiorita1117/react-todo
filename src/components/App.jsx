925294455eddad44b70f24b2bbe23f6c328670ff
折田幸夫 <3204704@gmail.com>
2020-01-24 10:37:18 +0900

    initialize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,60 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const App = props => {
-  const [state, setState] = useState(props);
-  const { name, price } = state;
-
-  useEffect(() => {
-    console.log("useEffect呼んでる！");
-  });
-
-  useEffect(() => {
-    console.log("componentDidMount");
-  }, []);
-
-  useEffect(() => {
-    console.log("this callback is for name");
-  }, [name]);
-
   return (
     <>
-      <p>
-        現在の{name}は{price}円持っています。
-      </p>
-      <button
-        onClick={() => {
-          setState({ ...state, price: price + 1 });
-        }}
-      >
-        +1
-      </button>
-      <button
-        onClick={() => {
-          setState({ ...state, price: price - 1 });
-        }}
-      >
-        -1
-      </button>
-      <button
-        onClick={() => {
-          setState(props);
-        }}
-      >
-        RESET
-      </button>
-      {/* e.targetで入力値を拾える */}
-      <input
-        value={name}
-        onChange={e => setState({ ...state, name: e.target.value })}
-      ></input>
+      <p>初期化</p>
     </>
   );
-};
-
-//defaultPropsはInitialStateと同義。Appの持つメソッド
-App.defaultProps = {
-  name: "JSON_HardCoder",
-  price: 1000
 };
 
 export default App;
