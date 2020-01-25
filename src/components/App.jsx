31e7c086ee184e5bcf84626d486ad4af4d245496
折田幸夫 <3204704@gmail.com>
2020-01-26 01:46:48 +0900

    fix bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -74,30 +74,8 @@ const App = () => {
           </thead>
           <tbody>
             {state.map((event, index) => (
-              <Event key={index} event={event} />
+              <Event key={index} event={event} dispatch={dispatch} />
             ))}
-            {/* {state.map((event, index) => {
-              const id = event.id;
-              const handleClickDeleteButton = () => {
-                dispatch({ type: "DELETE_EVENT", id });
-              };
-              return (
-                <tr key={index}>
-                  <td>{id}</td>
-                  <td>{event.title}</td>
-                  <td>{event.body}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="btn btn-danger"
-                      onClick={handleClickDeleteButton}
-                    >
-                      削除
-                    </button>
-                  </td>
-                </tr>
-              );
-            })} */}
           </tbody>
         </table>
       </div>
