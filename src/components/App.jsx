dc04f2a75aaedec329e77eb510f8070629a28ee3
折田幸夫 <3204704@gmail.com>
2020-01-25 14:03:09 +0900

    create form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = props => {
+const App = () => {
   return (
     <>
-      <p>初期化</p>
+      <div className="container">
+        <h4>イベント作成フォーム</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formEventTitle">Title</label>
+            <input className="form-control" id="formEventTitle"></input>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="formEventBody">Body</label>
+            <input className="form-control" id="formEventBody"></input>
+          </div>
+
+          <button className="btn btn-primary">イベント作成</button>
+          <button className="btn btn-danger">全てのイベントを削除する</button>
+        </form>
+      </div>
     </>
   );
 };
