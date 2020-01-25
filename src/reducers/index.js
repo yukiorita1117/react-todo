cd5e75c203aa4f1a1dcecda6cadbf7ce386f43af
折田幸夫 <3204704@gmail.com>
2020-01-25 15:17:00 +0900

    add event function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,21 @@
 //state:前の状態(未定義の可能性もあるので初期化する)
-const events = (state = [], action) => {};
+//サンプルイベント
+// event = [
+//   { id: 1, title: "タイトル1", body: "内容だよ" },
+//   { id: 1, title: "タイトル２", body: "内容だよ!!" }
+// ];
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      return state;
+    case "DELETE":
+      return state;
+    case "DELETE_ALL_EVENT":
+      return [];
+    default:
+      return state;
+  }
+};
 
 export default events;
