a61f9897605e6bb0f363b0703c76251e0b0f4702
折田幸夫 <3204704@gmail.com>
2020-01-26 03:54:09 +0900

    styling component

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import Event from "../Event";
+import styled from "styled-components";
+
+const EventsWrapper = styled.div`
+  margin-top: 24px;
+`;
 
 const Events = ({ state, dispatch }) => {
   return (
     <>
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Title</th>
-            <th scope="col">Body</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {state.map((event, index) => (
-            <Event key={index} event={event} dispatch={dispatch} />
-          ))}
-        </tbody>
-      </table>
+      <EventsWrapper>
+        <h4>イベント一覧</h4>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Body</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((event, index) => (
+              <Event key={index} event={event} dispatch={dispatch} />
+            ))}
+          </tbody>
+        </table>
+      </EventsWrapper>
     </>
   );
 };
