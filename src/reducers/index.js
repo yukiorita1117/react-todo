d815da247d0f6fffea37552410118d3016533ad1
折田幸夫 <3204704@gmail.com>
2020-01-26 03:49:42 +0900

    reducerも定数化

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,9 +5,11 @@
 //   { id: 1, title: "タイトル２", body: "内容だよ!!" }
 // ];
 
+import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENT } from "../actions";
+
 const events = (state = [], action) => {
   switch (action.type) {
-    case "CREATE_EVENT":
+    case CREATE_EVENT:
       console.log("reducer内部", state);
       const event = { title: action.title, body: action.body };
       const length = state.length;
@@ -15,9 +17,9 @@ const events = (state = [], action) => {
 
       //stateを展開して、id挿入し、eventも展開して挿入
       return [...state, { ...state, id, ...event }];
-    case "DELETE_EVENT":
+    case DELETE_EVENT:
       return state.filter(event => event.id !== action.id);
-    case "DELETE_ALL_EVENT":
+    case DELETE_ALL_EVENT:
       return [];
     default:
       return state;
